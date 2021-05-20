659debca9c5fd922b5654020e976f8e0bf92f0e6
Thomas William <thomaswpp02@gmail.com>
2021-05-20 01:50:14 -0300

    test: should add user with with complete data to maling list

--- src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -3,6 +3,7 @@ import { UserData } from './user-data'
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', () => {
     const users: UserData[] = []
+    console.log(users)
     // const repo: UserRepository = new InMemoryUserRepository(users)
     // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     // const name = 'any_name'
