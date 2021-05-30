80f2f12aa871c4d520f1e2a1887e4489a7bc0beb
Thomas William <thomaswpp02@gmail.com>
2021-05-30 17:45:00 -0300

    test: validate emails with more than 350 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -16,7 +16,12 @@ describe('email validation', () => {
     expect(Email.validate(email)).toBeTruthy()
   })
 
-  test('should not accept local part larger 64 chars', () => {
+  test('should not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(64) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
     const email = 'l'.repeat(65) + '@mail.com'
     expect(Email.validate(email)).toBeFalsy()
   })
