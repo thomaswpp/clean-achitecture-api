f3e1988fefdec21be5557b8afe373d8d05740487
Thomas William <thomaswpp02@gmail.com>
2021-05-30 17:32:43 -0300

    test: validate null and empty emails

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,19 @@
+import { Email } from './email'
+
+describe('email validation', () => {
+  test('should not accept null string', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept empty string', () => {
+    const email = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should accept valid email', () => {
+    const email = 'any@mail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
+
+})
