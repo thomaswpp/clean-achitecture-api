2e93f1c4caaf876489ce286e90dc0ff7a35b5cc8
Thomas William <thomaswpp02@gmail.com>
2021-06-01 01:17:17 -0300

    feat: improve error classes

--- src/entities/errors/invalid-name-error.ts
+++ src/entities/errors/invalid-name-error.ts
@@ -1,6 +1,6 @@
 export class InvalidNameError extends Error {
   public readonly name = 'InvalidNameError'
-  constructor(name: string) {
+  constructor (name: string) {
     super('Invalid name: ' + name + '.')
   }
 }
